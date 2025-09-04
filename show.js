58984667725c01a62eab01b4cfc0d6ff8ae24dc6
shajeeth <shajeethshajeeth5@gmail.com>
2025-09-04 12:17:03 +0530

    first commit

---
+++ show.js
@@ -0,0 +1,153 @@
+function show1(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./93c12d79da735856e9358e8306a402b2.jpg" alt="">
+        <a href="" >Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >Cooli</h1>
+        <p>Sunday 9.00PM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show2(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./MV5BNjY5OTg4NTYtZjVkZS00YTZmLWIwNDEtM2Y0ODQyMzM2NTJiXkEyXkFqcGc@._V1_.jpg" alt="">
+        <a href="" >Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >War 2</h1>
+        <p>Sunday 9.00PM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show3(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >Spider Man</h1>
+        <p>Sunday 4.00PM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show4(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./81RvPrkQycL._UF894,1000_QL80_.jpg" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >Trance formers the movie</h1>
+        <p>Sunday 4.00PM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show5(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./30dc6cd7a41fd5e25b5ce9415adec6eb.jpg" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >Avengers-End-Game</h1>
+        <p>Sunday 7.00PM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show6(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./4._Oppenheimer.format-webp.width-1440_6swN1ZFk46JTeIaq.webp" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >Oppenheimer</h1>
+        <p>Sunday 7.00PM on HBO</p>
+         <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show7(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="attachment-superman_returns_ver2_xlg.webp" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >superman-returns</h1>
+        <p>Sunday 10.00AM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show8(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./FeD319xWYAAW_KP.jpeg" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >God of War</h1>
+        <p>Sunday 10.00AM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
+function show9(){
+    document.getElementById("shows").innerHTML=`<section >
+   <div >
+    <div >
+        <img  src="./what-has-happened-to-movie-posters-v0-tm2y16wnnhb81.webp" alt="">
+        <a href="./marvsmposterbk_intdesign.jpg">Login to Subscribe</a>
+    </div>
+    <div >
+        <h1 >Uncharted</h1>
+        <p>Sunday 10.00AM on HBO</p>
+        <p>price 1200LKR</p>
+        <p>Wel come to all enjoy the show</p>
+    </div>
+   </div>
+   </section>`
+}
+
